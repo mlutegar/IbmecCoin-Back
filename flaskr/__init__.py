718b3771f983a06c08fdb4b2b1d26ff10263b17c
mlutegar <mlutegar@gmail.com>
2024-04-26 13:50:41 -0300

    atualizando o gitignore

---
+++ flaskr/__init__.py
@@ -0,0 +1,55 @@
+import os
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+import datetime
+
+def create_app(test_config=None): # test_config=None é um argumento opcional
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Inicializando o banco de dados
+    from . import db
+    db.init_app(app)
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    # Route for returning data
+    @app.route('/data')
+    def get_data():
+        """
+        Retorna dados para exibição em uma aplicação React.
+        """
+        # Obtendo a data e hora atual
+        x = datetime.datetime.now()
+
+        # Retornando dados no formato JSON
+        return jsonify({
+            'Name': "geek",
+            "Age": "22",
+            "Date": x.strftime("%Y-%m-%d %H:%M:%S"),
+            "Programming": "Python"
+        })
+
+    return app
